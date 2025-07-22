600c719dbf2bf2ed154c5bae7b44d18fde3c2c25
Lefie <qiwenruan11@gmail.com>
2025-07-22 16:30:20 -0400

    add a route to test home

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from app import create_app
 
 app = create_app()
 
+# simple test
+@app.route("/")
+def index():
+    return "hello from flask"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
